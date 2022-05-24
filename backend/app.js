773882bfa1838dc7113b2ffbddcdf9336378bca4
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-05-24 16:08:07 -0300

    a

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+require('dotenv').config()
+
+const router = require('./routes/Router');
+require('./config/db.js')
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log('App rodando na porta', port)
+});
