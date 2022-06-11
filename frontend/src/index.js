887eefb58266dc22c2090e62d6f086554f80d134
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-06-11 20:39:09 -0300

    a

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Redux
+import { Provider } from 'react';
+import {store} from './store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
