8cfbe621bd34dd8c7b3953ee3654112851668af4
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-06-23 18:57:31 -0300

    deploy 5

---
+++ frontend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const {resolve} = require('path');
+
+app.use('/', express.static(resolve(__dirname, './build')));
+
+app.listen(process.env.PORT || '3000', (err) => {
+    if(err){
+        return console.log(err)
+    }else{
+        return console.log('Tudo funcionando.')
+    }
+})
