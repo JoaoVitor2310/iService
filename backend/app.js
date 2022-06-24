4497c493fe53bc28c74e6be1e804f04b78c78344
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-06-24 15:17:12 -0300

    deploy

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.use('/', router);
 
-app.use('/', express.static(path.join(__dirname, '../frontend/build')));
+app.use('/', express.static(path.join(__dirname, '../frontend/App.js')));
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
