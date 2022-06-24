babd319dcc590dea0ecb3cf246eea49c08ad2e14
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-06-23 22:45:15 -0300

    deploy 13

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const {resolve} = require('path');
 
-app.use('/', express.static(resolve(__dirname, './src/App.js')));
+app.use('/', express.static(resolve(__dirname, './build')));
 
 app.listen(process.env.PORT || '3000', (err) => {
     if(err){
